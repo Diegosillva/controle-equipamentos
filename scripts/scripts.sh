39bdf1ae4103ce4e908984642a109423a9c26dfe
Diegosillva <diego.silperes@gmail.com>
2025-06-07 08:22:25 -0400

    test/validation script

---
+++ scripts/scripts.sh
@@ -0,0 +1,17 @@
+psql -U diego -d equipamento -f db.sql # para rodar um banco de dados atraves de um arquivo.
+
+# POST
+curl http://localhost:8080/equipamentos/api/v1/create \
+-H "Content-Type: application/json" \
+-d '{
+"produto":"Manila",
+"equipamento":"Prensa",
+"modelo":"BE-30",
+"numero_serie":"00000000000",
+"serial_dsp":"DSP-1020",
+"descricao":"Problema na chave fim de curso."
+}'
+
+
+# GET por produto
+curl "http://localhost:8080/equipamentos/api/v1/produto?nome=Manila"
