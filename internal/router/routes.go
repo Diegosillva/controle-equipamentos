9ff3567d7f83064d8b029baae1aa5857515dce6b
Diegosillva <diego.silperes@gmail.com>
2025-06-07 09:36:00 -0400

    add new routers

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -1,15 +1,16 @@
-package router 
+package router
 
-import(
-	"net/http"
+import (
 	"controle/internal/handler"
+	"net/http"
 )
 
-func Rotas()http.Handler{
+func Rotas() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler.HandlerHome)
+	mux.HandleFunc("/", handler.Home)
 	mux.HandleFunc("/equipamentos/api/v1/list", handler.GetEquipamentos)
 	mux.HandleFunc("/equipamentos/api/v1/produto", handler.GetByProdutoEquipamentos)
+	mux.HandleFunc("/equipamentos/api/v1/produto/delete", handler.DeleteByIdEquipamentos)
 	mux.HandleFunc("/equipamentos/api/v1/create", handler.PostEquipamentos)
 	return mux
 }
