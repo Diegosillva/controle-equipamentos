d9798cfd02bd7f36087a857b82fb7fd0dcad405b
Diegosillva <diego.silperes@gmail.com>
2025-06-09 18:38:08 -0400

    update

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,11 +1,13 @@
 package model
 
 type Equipamentos struct {
-	ID int `json:"id"`
-	Produto string `json:"produto"` 
-	Equipamento  string `json:"equipamento"`
-	Modelo string `json:"modelo"`
+	ID          int    `json:"id"`
+	Produto     string `json:"produto"`
+	Equipamento string `json:"equipamento"`
+	Modelo      string `json:"modelo"`
 	NumeroSerie string `json:"numero_serie"`
-	SerialDSP string `json:"serial_dsp"`
-	Descricao string `json:"descricao"`
+	SerialDSP   string `json:"serial_dsp"`
+	Localizacao string `json:"localizacao"`
+	Status      bool   `json:"status"`
+	Descricao   string `json:"descricao"`
 }
