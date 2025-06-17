4610d54036c84dd2f46b8b46b9d341733af00f4e
Diegosillva <diego.silperes@gmail.com>
2025-06-17 01:08:37 -0400

    created the router to edit

--- golang/internal/router/routes.go
+++ golang/internal/router/routes.go
@@ -10,14 +10,15 @@ import (
 func Rotas() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handler.Home)
-	mux.HandleFunc("/equipamentos/api/v1/list", handler.GetEquipamentos)
+	mux.HandleFunc("/equipamentos/api/v1/produto/list", handler.GetEquipamentos)
 	mux.HandleFunc("/equipamentos/api/v1/produto", handler.GetByProdutoEquipamentos)
 	mux.HandleFunc("/equipamentos/api/v1/produto/delete", handler.DeleteByIdEquipamentos)
-	mux.HandleFunc("/equipamentos/api/v1/create", handler.PostEquipamentos)
+	mux.HandleFunc("/equipamentos/api/v1/produto/create", handler.PostEquipamentos)
+	mux.HandleFunc("/equipamentos/api/v1/produto/edit", handler.UpdateEquipamentos)
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodDelete},
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodPut},
 		AllowCredentials: true,
 	})
 
